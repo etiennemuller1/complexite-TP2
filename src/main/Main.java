16fa5a1f5e302b857477e2586c2dfac7da046a01
p19020624 <axel.poulain@etu.univ-amu.fr>
2022-11-16 16:38:50 +0100

    Performance: Nettoyage

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import Performance.Performance;
+
 import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -10,5 +12,15 @@ public class Main {
         System.out.println(verificateur.verifier());*/
         Sudoku sudoku = new Sudoku("/amuhome/m16014784/Bureau/Reduction/src/sudoku.txt");
         sudoku.toSAT();
+    }
+
+    public static void performance() {
+        int nbOfMeasures = 5;
+        int upTo = 5_000;
+
+        Performance.exportFile(Performance.getVerifTautologyPerf(upTo, nbOfMeasures),
+                               "VerifTaut" + upTo + "_" + nbOfMeasures + ".txt");
+        Performance.exportFile(Performance.getVerifContradictionPerf(upTo, nbOfMeasures),
+                "VerifContra" + upTo + "_" + nbOfMeasures + ".txt");
     }
 }
