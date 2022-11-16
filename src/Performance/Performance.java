d5af9565926c859c84fad27f5df1c705b1f7486c
p19020624 <axel.poulain@etu.univ-amu.fr>
2022-11-16 16:35:44 +0100

    Performance: Nettoyage

--- src/Performance/Performance.java
+++ src/Performance/Performance.java
@@ -12,46 +12,6 @@ public class Performance {
 
     final static int NANOSECONDS_TO_SECONDS = 1_000_000_000;
 
-    /** Représente un résultat de test de performance */
-    public static class PerformanceResult {
-        public int[] indexes;
-        public double[] results;
-
-        public PerformanceResult(int size) {
-            this.indexes = new int[size];
-            this.results = new double[size];
-        }
-
-        /** Exporte les résultats sous forme de fichier
-         * La première colonne représente l'index, la deuxième
-         * le temps associé
-         *
-         * @param filename Le nom du fichier
-         */
-        public void toFile(String filename) {
-            FileWriter writer;
-            try {
-                writer = new FileWriter(filename, false);
-            } catch (IOException e) {
-                System.out.println("Error when exporting the benchmark file.");
-                return;
-            }
-
-            try {
-                for (int i = 0; i < indexes.length; i++) {
-                    writer.write(indexes[i] + " " + results[i] + "\n");
-                }
-            } catch (IOException e) {
-                System.out.println("Error when writing to the benchmark file.");
-                return;
-            }
-
-            try { writer.close(); } catch (IOException e) {
-                System.out.println("Error when closing the writer.");
-            }
-        }
-    }
-
     /** Calcule les performances d'un algorithme
      *
      * @param upTo Jusqu'à quelle taille tester, inclus
@@ -94,7 +54,7 @@ public class Performance {
         return getPerformance(upTo, nbOfMeasures, func);
     }
 
-    private static Double[] getVerifContradictionPerf(int upTo, int nbOfMeasures) {
+    public static Double[] getVerifContradictionPerf(int upTo, int nbOfMeasures) {
         IntToLongFunction func = (size) -> {
             Instant before, after;
             Verificateur.Formule formule = Verificateur.Formule.generateContradiction(size);
