f274c533f3beb0a6cecc04df9c95b5b4a561e988
Axel <axel.poulain@etu.univ-amu.fr>
2022-11-21 23:31:14 +0100

    Performance: Tests Stable
    On risque d'avoir des problèmes pour les tests, puisqu'il faudra tester
    les solveurs sur exactement la même instance de graphe que lors des
    tests de réduction ; or les graphes générés sont détruits à la fin de
    chaque tests… À réfléchir.

--- src/Performance/Performance.java
+++ src/Performance/Performance.java
@@ -1,6 +1,7 @@
 package Performance;
 
 import main.Formule;
+import main.Stable;
 import main.Verificateur;
 
 import java.io.FileWriter;
@@ -63,6 +64,23 @@ public class Performance {
 
             before = Instant.now();
             formule.verify(affectations);
+            after = Instant.now();
+
+            return before.until(after, ChronoUnit.NANOS);
+        };
+
+        return getPerformance(upTo, nbOfMeasures, func);
+    }
+
+    final static private double PERF_GRAPH_DENSITY = 0.7;
+    public static Double[] getStablePerf(int upTo, int nbOfMeasures) {
+        IntToLongFunction func = (size) -> {
+            Instant before, after;
+            Stable.Graph graph = Stable.Graph.generateRandomGraph(size, PERF_GRAPH_DENSITY);
+            Stable stable = new Stable(graph);
+
+            before = Instant.now();
+            stable.computeAndGetFormula();
             after = Instant.now();
 
             return before.until(after, ChronoUnit.NANOS);
