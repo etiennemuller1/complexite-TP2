a873fb113054019488f44713d930e8be021c57d6
Axel <axel.poulain@etu.univ-amu.fr>
2022-11-21 18:51:58 +0100

    Stable: Début contrainte sur les voisins des sommets

--- src/main/Stable.java
+++ src/main/Stable.java
@@ -1,8 +1,24 @@
 package main;
 
+import java.util.ArrayList;
+
 /** Construit la réduction d'un problème zone-vide vers SAT */
 public class Stable {
     private Formule clauses;
 
+    /** Rajoute une contrainte de voisinage pour le sommet et son voisin donnés
+     *  (si on prend le sommet, alors on ne prend pas son voisin)
+     *  Voir rapport pour plus de détails
+     *
+     * @param variableSommet Le sommet
+     * @param variableVoisin Son voisin
+     */
+    private void addEdgeConstraint(int variableSommet, int variableVoisin) {
+        ArrayList<Integer> constraint = new ArrayList<>(2);
+        constraint.add(-variableSommet);
+        constraint.add(-variableVoisin);
 
+        clauses.addClause(constraint);
+    }
+    /*TODO: Trèèès probablement à changer, comme un seul sommet ne va pas correspondre à une seule variable */
 }
