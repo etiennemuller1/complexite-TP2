f66f2c8e2d1d938b6d48115ddf89ffbd6fbebbd8
p19020624 <axel.poulain@etu.univ-amu.fr>
2022-11-29 18:59:33 +0100

    Clause: Constructeurs

--- src/main/Clause.java
+++ src/main/Clause.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Clause implements Iterable<Integer> {
-    ArrayList<Integer> clauseParts = new ArrayList<>();
+    ArrayList<Integer> clauseParts;
+
+    /** Créée une nouvelle clause */
+    public Clause() {
+        this.clauseParts = new ArrayList<>();
+    }
+
+    /** Créée une nouvelle clause optimisée pour le nombre d'éléments spécifiés
+     *
+     * @param size La taille d'éléments que devra contenir cette clause
+     *             Elle sera redimensionnée automatiquement si besoin est
+     */
+    public Clause(int size) {
+        this.clauseParts = new ArrayList<>(size);
+    }
 
     public void add(int clausePart) {
         clauseParts.add(clausePart);
